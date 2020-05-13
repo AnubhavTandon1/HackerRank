b2b7d7b7cc9c95f5c9027c96434fa51bf24e3021
AnubhavTandon1 <63805257+AnubhavTandon1@users.noreply.github.com>
2020-05-14 00:34:02 +0530

    Add files via upload

---
+++ Day 1/Handson 3/Solution.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = scanner.nextInt();
+        }
+
+        int[] rotated = new int[n];
+        for(int i=0; i < n; i++){
+            rotated[(i+n-k) % n] = a[i];
+        }
+
+        for(int i=0; i < n; i++){
+            System.out.print(rotated[i] + " ");
+        }
+        scanner.close();
+    }
+}
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = scanner.nextInt();
+        }
+
+        int[] rotated = new int[n];
+        for(int i=0; i < n; i++){
+            rotated[(i+n-k) % n] = a[i];
+        }
+
+        for(int i=0; i < n; i++){
+            System.out.print(rotated[i] + " ");
+        }
+        scanner.close();
+    }
+}
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = scanner.nextInt();
+        }
+
+        int[] rotated = new int[n];
+        for(int i=0; i < n; i++){
+            rotated[(i+n-k) % n] = a[i];
+        }
+
+        for(int i=0; i < n; i++){
+            System.out.print(rotated[i] + " ");
+        }
+        scanner.close();
+    }
+}
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = scanner.nextInt();
+        }
+
+        int[] rotated = new int[n];
+        for(int i=0; i < n; i++){
+            rotated[(i+n-k) % n] = a[i];
+        }
+
+        for(int i=0; i < n; i++){
+            System.out.print(rotated[i] + " ");
+        }
+        scanner.close();
+    }
+}
